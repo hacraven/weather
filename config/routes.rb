e9899f6e67a39eb46627d8c6c17279eeea15972b
hacraven <hacraven1@gmail.com>
2018-07-16 09:52:01 -0400

    routes clarify

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/test'
-  get 'welcome/index'
+  
+  root 'welcome#index'
+  get 'index' => 'welcome#index'
+  post 'index' => 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'test' => 'welcome#test'
+
 end
