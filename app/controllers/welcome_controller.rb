81617c97dff3d925f5538a9425b3bd2e19ef4e96
hacraven <hacraven1@gmail.com>
2018-07-16 08:51:16 -0400

    welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
 class WelcomeController < ApplicationController
   def test
+  	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/AZ/Flagstaff.json")
+  
+  	@location = response['location']['city']
+  	@temp_f = response['current_observation']['temp_f']
+  	@temp_c = response['current_observation']['temp_c']
+  
+  	@weather_icon = response['current_observation']['icon_url']
+  	@weather_words = response['current_observation']['weather']
+  	@forecast_link = response['current_observation']['forecast_url']
+  	@real_feel = response['current_observation']['']
+
   end
 
   def index
