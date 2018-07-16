ff9dfc8bf9f89396bacf767f4f13901e2ae05c84
hacraven <hacraven1@gmail.com>
2018-07-16 10:29:59 -0400

    finished index so that it would show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,7 +17,7 @@ class WelcomeController < ApplicationController
   	@states = %w(AL AK AZ AR CA CO CT DE FL GA SC NC VA WV MD PA NJ NY RI MA ME NH VT OH MI IN IL KY TN MS LA DC MO IA WI MN ND SD NE KS OK TX NM WY MT ID UT NV OR WA HI).sort!
  		
  		if params[ :city] != nil
- 			 params[:city].gsub(" ", "_")
+ 			 params[:city].gsub!(" ", "_")
 
   end
 
@@ -26,7 +26,7 @@ class WelcomeController < ApplicationController
 
   	if response['response']['error'] == nil || response['error']
 
-  	if response.key?["location"]	
+  	if response["location"] != nil
   		@location = response['location']['city']
 	  	@temp_f = response['current_observation']['temp_f']
 	  	@temp_c = response['current_observation']['temp_c']
@@ -34,30 +34,17 @@ class WelcomeController < ApplicationController
 	  	@weather_icon = response['current_observation']['icon_url']
 	  	@weather_words = response['current_observation']['weather']
 	  	@forecast_link = response['current_observation']['forecast_url']
-	  	@real_feel = response['current_observation']['feelslike_strings']
+	  	@real_feel_f = response['current_observation']['feelslike_f']
+	  	@real_feel_c = response['current_observation']['feelslike_c']
 
-
-  	else
-  		@error = "For some reason your City/State combo does not exsist.  Yes, I know that 
-  		you know what you are talking about---but I apparently don't--so try again!"	
-
+	  	else
+	  		@error = "For some reason your City/State combo does not exsist.  Yes, I know that 
+	  		you know what you are talking about---but I apparently don't--so try again!"	
+	  	end
   	else
   		@error = response['response']['error']['description']
+  	end
+
+	  end	
+  end		
 end
-
-# class WelcomeController < ApplicationController
-#   def test
-#   	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/AZ/Flagstaff.json")
-
-#   	  @location = response['location']['city']
-#   	@temp_f = response['current_observation']['temp_f']
-#   	@temp_c = response['current_observation']['temp_c']
-#   	@weather_icon = response['current_observation']['icon_url']
-#   	@weather_words = response['current_observation']['weather']
-#   	@forecast_link = response['current_observation']['forecast_url']
-#   	@real_feel = response['current_observation']['feelslike_string']
-#   end
-
-#   def index
-#   end
-# end
